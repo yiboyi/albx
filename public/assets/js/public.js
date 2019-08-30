4095612c41e4b147dd96a4290257d9f87b35b2c9
yifeng <793173580@qq.com>
2019-08-30 14:33:43 +0800

    索要文章分类列表数据

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -46,4 +46,25 @@ $.ajax({
         var html = template.render(commentTpl, { data: response });
         $('#commentBox').html(html);
     }
+});
+
+// 向服务器端发送请求 索要文章分类列表数据
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function(response) {
+        var navTpl = `
+			{{each data}}
+			<li>
+				<a href="list.html?categoryId={{$value._id}}">
+					<i class="fa {{$value.className}}"></i>{{$value.title}}
+				</a>
+			</li>
+			{{/each}}
+		`;
+        var html = template.render(navTpl, { data: response });
+        // $('#navBox').html(html)
+        // $('#topNavBox').html(html)
+        $('.nav_item').html(html)
+    }
 })
